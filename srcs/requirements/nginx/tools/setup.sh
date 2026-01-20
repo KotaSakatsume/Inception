3b03dfbf245ae54625581a23e308cd755093d4d4
Kouta Sakatsume <kosakats@c6r1s7.42tokyo.jp>
2026-01-20 15:25:23 +0900

    updata

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -8,16 +8,12 @@ if [ ! -f "$SSL_DIR/nginx.crt" ]; then
 
 	#証明書（x509形式）作成
 	openssl req -x509 -nodes \
-		#期限
 		-days 365 \
-		#鍵作成
 		-newkey rsa:2048 \
-		#鍵の置き場
 		-keyout $SSL_DIR/nginx.key \
-		#証明書の置き場
 		-out $SSL_DIR/nginx.crt \
-		#-subj "/C=JP/ST=Tokyo/L=Tokyo/O=42/OU=student/CN=kosakats.42.fr"
-		-subj "/C=JP/ST=Tokyo/L=Tokyo/O=42/OU=student/CN=$DOMAIN_NAME"
+		-subj "/C=JP/ST=Tokyo/L=Tokyo/O=42/OU=student/CN=kosakats.42.fr"
+		#-subj "/C=JP/ST=Tokyo/L=Tokyo/O=42/OU=student/CN=$DOMAIN_NAME"
 
 fi
 
